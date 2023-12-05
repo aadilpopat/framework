f4ac4b8850f2b70695bc6012ca49450767c63d12
Aadil Popat <aadilpopat@hotmail.co.uk>
2023-12-05 23:05:13 +0000

    fixes

--- wp-content/themes/framework-theme/template-parts/partials/accordion.php
+++ wp-content/themes/framework-theme/template-parts/partials/accordion.php
@@ -1,10 +1,9 @@
 <div class='accordion container md:grid md:grid-cols-12'>
 
 	<div class='col-span-5 col-start-2'>
-	<h3 class='text-heading-large text-green-100 mb-20'>Heading text here...</h3>
+	<h3 class='text-heading-large text-green-100 mb-20'><?= get_field('heading')?></h3>
 
-	<p class='text-copy text-green-100 mb-[3.12rem]'>Lorem ipsum dolor sit amet consectetur. Malesuada sit consequat
-		tristique consectetur sit tristique amet in. Etiam egestas elit vestibulum commodo.</p>
+	<p class='text-copy text-green-100 mb-[3.12rem]'><?= get_field('text')?></p>
 	</div>
 
 	<div class='col-span-10 col-start-2'>
@@ -13,7 +12,7 @@
 				<summary class="flex flex-nowrap justify-between w-full place-items-baseline cursor-pointer border-b border-b-white-300 md:grid md:grid-cols-10">
 							<span class='text-copy inline-block mr-30 md:mr-0 md:col-span-1'><?= sprintf('%02d', $x+1); ?></span>
 							<h5 class='text-heading-xsmall text-green-100 mb-26 md:col-span-8 md:mb-8'>
-								Lorem ipsum dolor sit amet consectetur adipisicing?
+								<?= get_field('section_heading')?>
 							</h5>
 						<div class="relative w-21 h-21 flex items-center mt-8 ml-37 md:mt-0 md:ml-0 md:mr-16 md:col-span-1 md:grid-col-none">
 							<span class='absolute right-0 opacity-100 group-open:opacity-0'>
@@ -27,10 +26,7 @@
 				<div class='md:grid md:grid-cols-10'>
 					<div class='md:col-span-8 md:col-start-2'>
 						<p class='mt-30 text-copy text-green-100'>
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-							molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-							voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-							earum similique!
+							<?= get_field('section_text')?>
 						</p>
 					</div>
 				</div>
